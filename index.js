9b47748969dbca6b874176e12ce2af9927f2c98d
R3F1 <refiak99@gmail.com>
2026-04-28 11:35:21 +0200

    Refactor Steam authentication and add player search API

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 const express = require('express');
+const axios = require('axios'); 
 const app = express();
 
-// Strona testowa dla przeglądarki
-app.get('/', (req, res) => {
-  res.send('<h1 style="font-family: sans-serif; color: #1A73E8; text-align: center; margin-top: 20%;">Serwer CS2 REFITV API Dziala i nasluchuje!</h1>');
-});
+// Twój klucz API
+const STEAM_API_KEY = '531A9DA59816EE4D594BD29F82EEEDE5';
 
-// Główny endpoint logowania
+app.get('/', (req, res) => res.send('API REFITV ONLINE - Gotowe na zapytania!');
+
+// 1. LOGOWANIE STEAM
 app.get('/auth/steam', (req, res) => {
   const appReturnUrl = req.query.appUrl; 
   const host = req.get('host');
   const serverReturnUrl = `https://${host}/auth/steam/return?appUrl=${encodeURIComponent(appReturnUrl)}`;
-
   const steamLoginUrl = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(serverReturnUrl)}&openid.realm=${encodeURIComponent(`https://${host}`)}&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
-
   res.redirect(steamLoginUrl);
 });
 
-// Powrót ze Steama i odesłanie do aplikacji
 app.get('/auth/steam/return', (req, res) => {
   const claimedId = req.query['openid.claimed_id'];
   const appReturnUrl = req.query.appUrl;
-
   if (claimedId && appReturnUrl) {
     const steamId = claimedId.split('/').pop();
-    const finalRedirectUrl = `${appReturnUrl}?steamid=${steamId}`;
-    res.redirect(finalRedirectUrl);
+    res.redirect(`${appReturnUrl}?steamid=${steamId}`);
   } else {
-    res.send('Blad: Brak danych ze Steam. Sprobuj zalogowac sie ponownie.');
+    res.send('Blad logowania');
+  }
+});
+
+// 2. WYSZUKIWARKA GRACZY (Pobiera PRAWDZIWE dane z Valve)
+app.get('/api/player/:id', async (req, res) => {
+  try {
+    const response = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${req.params.id}`);
+    const playerData = response.data.response.players[0];
+    
+    if (playerData) {
+      res.json(playerData); // Odsyłamy dane z powrotem do telefonu
+    } else {
+      res.status(404).json({ error: 'Nie znaleziono gracza' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Blad polaczenia z serwerami Valve' });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Serwer wystartowal na porcie ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Serwer startuje na porcie ${PORT}`));
