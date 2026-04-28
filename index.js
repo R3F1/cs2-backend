f62491015209faac7838f83b85a0aeef4839d3e6
R3F1 <refiak99@gmail.com>
2026-04-28 11:06:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Strona testowa dla przeglądarki
 app.get('/', (req, res) => {
-  res.send('<h1>Serwer CS2 REFITV Dziala!</h1>');
+  res.send('<h1 style="font-family: sans-serif; color: #1A73E8; text-align: center; margin-top: 20%;">Serwer CS2 REFITV API Dziala i nasluchuje!</h1>');
 });
 
+// Główny endpoint logowania
 app.get('/auth/steam', (req, res) => {
   const appReturnUrl = req.query.appUrl; 
-  const host = req.get('host'); // Tu bedzie Twoja domena refitv.pl
+  const host = req.get('host');
   const serverReturnUrl = `https://${host}/auth/steam/return?appUrl=${encodeURIComponent(appReturnUrl)}`;
 
   const steamLoginUrl = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(serverReturnUrl)}&openid.realm=${encodeURIComponent(`https://${host}`)}&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
@@ -15,6 +17,7 @@ app.get('/auth/steam', (req, res) => {
   res.redirect(steamLoginUrl);
 });
 
+// Powrót ze Steama i odesłanie do aplikacji
 app.get('/auth/steam/return', (req, res) => {
   const claimedId = req.query['openid.claimed_id'];
   const appReturnUrl = req.query.appUrl;
@@ -24,11 +27,11 @@ app.get('/auth/steam/return', (req, res) => {
     const finalRedirectUrl = `${appReturnUrl}?steamid=${steamId}`;
     res.redirect(finalRedirectUrl);
   } else {
-    res.send('Blad: Brak danych ze Steam.');
+    res.send('Blad: Brak danych ze Steam. Sprobuj zalogowac sie ponownie.');
   }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Serwer biega na porcie ${PORT}`);
+  console.log(`Serwer wystartowal na porcie ${PORT}`);
 });
